968026b2567d416e4f5aa4ba8b09e60b0c94f8ff
Roger Hutchings <roger@hutchin.gs>
2015-01-07 16:42:46 +0000

    Add svg ref, get dummy click events working

--- app/directives/text-tool.js
+++ app/directives/text-tool.js
@@ -14,6 +14,7 @@
                 link: function (scope, element, attrs) {
 
                     var ClassifyCtrl = scope.$parent;
+                    var svg = angular.element(ClassifyCtrl.svg);
 
                     scope.toggle = function () {
                         ClassifyCtrl.setTool(scope.tool);
@@ -26,10 +27,16 @@
 
                         activate: function () {
                             console.log(this.name, 'active');
+                            svg.on('click', this.click.bind(this));
                         },
 
                         deactivate: function () {
                             console.log(this.name, 'inactive');
+                            svg.off('click');
+                        },
+
+                        click: function (event) {
+                            console.log('click', event);
                         }
 
                     };
