cbfbccd7995bde71b9da4a7fc2be99d29566bdbd
Roger Hutchings <roger@hutchin.gs>
2015-01-09 12:36:04 +0000

    Add unstyled text annotation markers

---
+++ app/directives/text-annotation.js
@@ -0,0 +1,38 @@
+(function (angular) {
+
+    'use strict';
+
+    var app = angular.module('app');
+
+    app.directive('textAnnotation', [
+        'Config',
+        function (Config) {
+            return {
+                scope: {
+                    data: '='
+                },
+                restrict: 'A',
+                replace: true,
+                templateUrl: 'directives/text-annotation.html',
+                link: function (scope, element, attrs) {
+
+                    var ClassifyCtrl = scope.$parent.$parent;
+                    var panZoom = ClassifyCtrl.panZoom;
+                    var viewport = angular.element(ClassifyCtrl.viewport);
+
+                    scope.r = Config.svg.pointSize;
+
+                    scope.addHoverClass = function () {
+                        element.addClass('hover');
+                    };
+
+                    scope.removeHoverClass = function () {
+                        element.removeClass('hover');
+                    };
+
+                }
+            }
+        }
+    ]);
+
+}(window.angular));
