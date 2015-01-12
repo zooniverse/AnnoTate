933f599a7ee432d83688140f05b8e7b7d55b6840
Roger Hutchings <roger@hutchin.gs>
2015-01-12 10:59:45 +0000

    Add temporary image annotation

--- app/directives/image-tool.js
+++ app/directives/image-tool.js
@@ -7,7 +7,8 @@
     module.directive('imageTool', [
         '$log',
         'hotkeys',
-        function ($log, hotkeys) {
+        'AnnotationsFactory',
+        function ($log, hotkeys, Annotations) {
             return {
                 restrict: 'E',
                 replace: true,
@@ -33,13 +34,39 @@
 
                         name: 'Image',
                         icon: 'picture',
+                        tempRect: null,
 
                         activate: function () {
+                            viewport.on('mousedown', this.startDraw.bind(this));
+                            viewport.on('mouseup', this.finishDraw.bind(this));
                             $log.info(this.name, 'activated');
                         },
 
                         deactivate: function () {
+                            viewport.off('mousedown', this.startDraw);
+                            viewport.off('mouseup', this.finishDraw);
                             $log.info(this.name, 'deactivated');
+                        },
+
+                        startDraw: function (event) {
+                            event.stopImmediatePropagation();
+                            this.tempRect = Annotations.add(_.extend(ClassifyCtrl.getPoint(event), {
+                                type: 'tempImage',
+                                width: 0,
+                                height: 0
+                            }));
+                            viewport.on('mousemove', this.draw.bind(this));
+                        },
+
+                        draw: function (event) {
+                            var newPoint = ClassifyCtrl.getPoint(event);
+                            this.tempRect.width = newPoint.x - this.tempRect.x;
+                            this.tempRect.height = newPoint.y - this.tempRect.y;
+                            scope.$apply();
+                        },
+
+                        finishDraw: function (event) {
+                            viewport.off('mousemove');
                         }
 
                     };
