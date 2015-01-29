fa293fbd717d445c05dc793e84ccace16690f9fa
Roger Hutchings <roger@hutchin.gs>
2015-01-29 14:32:41 +0000

    Rotation... kind of works

--- app/directives/svg-pan-zoom.js
+++ app/directives/svg-pan-zoom.js
@@ -5,7 +5,8 @@
     var module = angular.module('app');
 
     module.directive('svgPanZoom', [
-        function () {
+        '$log',
+        function ($log) {
             return {
                 restrict: 'A',
                 link: function (scope, element, attrs) {
@@ -17,9 +18,12 @@
                         fit: false
                     });
                     scope.viewport = svg.getElementsByClassName('svg-pan-zoom_viewport')[0];
+                    var rotateContainer = svg.getElementsByClassName('rotate-container')[0];
+
+                    scope.rotation = 0;
 
                     scope.getPoint = function (event) {
-                        var rect = scope.viewport.getBoundingClientRect();
+                        var rect = rotateContainer.getBoundingClientRect();
                         var zoom = parseFloat(scope.panZoom.getSizes().realZoom);
                         return {
                            x: Math.round((event.clientX - rect.left) / zoom),
@@ -35,8 +39,17 @@
                     };
 
                     scope.rotate = function (degrees) {
-                        var rotateContainer = svg.getElementsByClassName('rotate-container');
-                        console.log(rotateContainer);
+                        degrees = degrees || 0;
+                        $log.log('Rotating', rotateContainer, { current: scope.rotation, delta: degrees, new: scope.rotation + degrees});
+
+                        scope.rotation = scope.rotation + degrees;
+
+                        var rect = rotateContainer.getBoundingClientRect();
+                        $log.log(rect)
+                        var transform = [scope.rotation, rect.width / 2, rect.height / 2];
+
+                        angular.element(rotateContainer).attr('transform', 'rotate(' + transform.join(' ') + ')');
+
                     };
 
                 }
