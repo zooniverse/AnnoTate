28551196b1d083bdd6f077959527ae6a79cee907
Roger Hutchings <roger@hutchin.gs>
2015-04-30 15:11:32 +0100

    Toggle toolbar buttons

--- app/modules/transcribe/toolbar.directive.js
+++ app/modules/transcribe/toolbar.directive.js
@@ -12,33 +12,56 @@ function transcribeToolbar() {
         restrict: 'A',
         replace: true,
         templateUrl: 'transcribe/toolbar.html',
-        link: transcribeToolbarLink
+        link: transcribeToolbarLink,
+        controller: transcribeToolbarController
     };
     return directive;
 
-    function transcribeToolbarLink(scope) {
+    function transcribeToolbarController() {
+
+        var vm = this;
+
+        vm.activeTool = null;
+
+        vm.tools = {
+            text: { name: 'text' },
+            image: { name: 'image' }
+        };
+
+    }
+
+    function transcribeToolbarLink(scope, element, attrs, ctrl) {
 
         var transcribeCtrl = scope.$parent;
 
         scope.rotate = rotate;
         scope.centre = centre;
-        scope.activate = activate;
+        scope.toggle = toggle;
         scope.next = next;
 
         function rotate(value) {
-            transcribeCtrl.$emit('rotate', value);
+            transcribeCtrl.$broadcast('rotate', value);
         }
 
         function centre() {
-            transcribeCtrl.$emit('centre');
+            transcribeCtrl.$broadcast('centre');
         }
 
-        function activate() {
-            transcribeCtrl.$emit('activate');
+        function toggle(toolName) {
+            var newTool;
+
+            if (ctrl.activeTool && ctrl.activeTool.name === toolName) {
+                newTool = null;
+            } else {
+                newTool = ctrl.tools[toolName];
+            }
+
+            ctrl.activeTool = newTool;
+            transcribeCtrl.$broadcast('setTool', newTool);
         }
 
         function next() {
-            transcribeCtrl.$emit('next');
+            transcribeCtrl.$broadcast('next');
         }
 
     }
