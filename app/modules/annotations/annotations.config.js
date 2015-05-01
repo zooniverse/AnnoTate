01f9cd1928e2c382e366919a91fa296f022ccd79
Roger Hutchings <roger@hutchin.gs>
2015-05-01 22:41:58 +0100

    Add annotations config

---
+++ app/modules/annotations/annotations.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var annotationsConfig = {
+    pointRadius: 10
+};
+
+module.exports = {
+    constants: annotationsConfig
+};
--- app/modules/annotations/annotations.module.js
+++ app/modules/annotations/annotations.module.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var annotationsConfig = require('./annotations.config.js');
+
 // A module for the static pages such as home, about etc.
 module.exports = require('angular')
-    .module('app.annotations', []);
+    .module('app.annotations', [])
+    .constant('annotationsConfig', annotationsConfig.constants);
