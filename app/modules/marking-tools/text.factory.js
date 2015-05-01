dedf95bfe2553430ae8eb4a1197e15e5060b4ad5
Roger Hutchings <roger@hutchin.gs>
2015-05-01 22:44:05 +0100

    Add remaining functionality to text tool

--- app/modules/marking-tools/text.factory.js
+++ app/modules/marking-tools/text.factory.js
@@ -3,10 +3,12 @@
 require('./marking-tools.module.js')
     .factory('textTool', textTool);
 
+var _ = require('lodash');
+
 /**
  * @ngInject
  */
-function textTool(Annotations, toolUtils) {
+function textTool($rootScope, Annotations, toolUtils) {
 
     var factory;
     var _svg;
@@ -25,37 +27,49 @@ function textTool(Annotations, toolUtils) {
         _svg = svg;
         _subject = svg.find('.subject');
         _subject.on('mousedown.text', _clickHandler);
+        _startPoint = null;
     }
 
     function deactivate() {
+        if (_startPoint)
+            _clearStartPoint();
         _subject.off('.text');
     }
 
     function _clickHandler(event) {
         _subject.on('mouseup.text mousemove.text', clickOrDrag);
         function clickOrDrag(event) {
-            if (event.type === 'mouseup') {
-                if (!_startPoint) {
-                   _addStartPoint(event);
-                } else {
-                    _addAnnotation(event);
-                    _removeStartPoint();
-                }
-            }
+            if (event.type === 'mouseup')
+                (!_startPoint) ? _startLine(event) : _endLine(event);
             _subject.off('mouseup.text mousemove.text', clickOrDrag);
         }
     }
 
-    function _addStartPoint(event) {
-        console.log(toolUtils.getPoint(_svg, event))
+    function _startLine(event) {
+        var point = toolUtils.getPoint(_svg, event);
+        _startPoint = Annotations.add(_.extend(point, {
+            type: 'tempText',
+            temp: true
+        }));
+        $rootScope.$apply();
     }
 
-    function _addAnnotation() {
-
+    function _endLine(event) {
+        var endPoint = toolUtils.getPoint(_svg, event);
+        Annotations.add({
+            type: 'text',
+            x1: _startPoint.x,
+            y1: _startPoint.y,
+            x2: endPoint.x,
+            y2: endPoint.y
+        });
+        _clearStartPoint();
+        $rootScope.$apply();
     }
 
-    function _removeStartPoint(event) {
-        console.log('remove')
+    function _clearStartPoint() {
+        Annotations.destroy(_startPoint);
+        _startPoint = null;
     }
 
 }
