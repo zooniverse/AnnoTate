6d061eb0431d4afc411e2f33dfd1ac5745d9e7e1
Roger Hutchings <roger@hutchin.gs>
2015-05-02 22:27:17 +0100

    Allow functions to be used before declaration in hinting

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -4,8 +4,12 @@ var config = require('../config');
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 
+var jshintOptions = {
+    latedef: 'nofunc'
+};
+
 gulp.task('lint', function() {
   return gulp.src([config.scripts.src, '!app/modules/**/*.templates.js'])
-    .pipe(jshint())
+    .pipe(jshint(jshintOptions))
     .pipe(jshint.reporter('jshint-stylish'));
 });
