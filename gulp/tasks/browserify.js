eb7831851657f39c571eae87d9ad46d9b22d872d
Roger Hutchings <roger@hutchin.gs>
2015-05-12 13:58:14 +0100

    Formatting

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,77 +1,74 @@
 'use strict';
 
-var config       = require('../config');
-var gulp         = require('gulp');
-var gulpif       = require('gulp-if');
-var gutil        = require('gulp-util');
-var source       = require('vinyl-source-stream');
-var sourcemaps   = require('gulp-sourcemaps');
-var buffer       = require('vinyl-buffer');
-var streamify    = require('gulp-streamify');
-var watchify     = require('watchify');
-var browserify   = require('browserify');
-var babelify     = require('babelify');
-var uglify       = require('gulp-uglify');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+var buffer = require('vinyl-buffer');
+var config = require('../config');
+var debowerify = require('debowerify');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
 var handleErrors = require('../util/handleErrors');
-var browserSync  = require('browser-sync');
-var debowerify   = require('debowerify');
-var ngAnnotate   = require('browserify-ngannotate');
+var ngAnnotate = require('browserify-ngannotate');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var watchify = require('watchify');
 
 // Based on: http://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
 function buildScript(file) {
 
-  var bundler = browserify({
-    entries: config.browserify.entries,
-    debug: true,
-    cache: {},
-    packageCache: {},
-    fullPaths: true
-  }, watchify.args);
+    var bundler = browserify({
+        entries: config.browserify.entries,
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    }, watchify.args);
 
-  if ( !global.isProd ) {
-    bundler = watchify(bundler);
-    bundler.on('update', function() {
-      rebundle();
+    if (!global.isProd) {
+        bundler = watchify(bundler);
+        bundler.on('update', function() {
+            rebundle();
+        });
+    }
+
+    var transforms = [
+        babelify,
+        debowerify,
+        ngAnnotate,
+        'brfs',
+        'bulkify',
+        'browserify-shim'
+    ];
+
+    transforms.forEach(function(transform) {
+        bundler.transform(transform);
     });
-  }
 
-  var transforms = [
-    babelify,
-    debowerify,
-    ngAnnotate,
-    'brfs',
-    'bulkify',
-    'browserify-shim'
-  ];
+    function rebundle() {
+        var stream = bundler.bundle();
+        var createSourcemap = global.isProd && config.browserify.sourcemap;
 
-  transforms.forEach(function(transform) {
-    bundler.transform(transform);
-  });
+        gutil.log('Rebundle...');
 
-  function rebundle() {
-    var stream = bundler.bundle();
-    var createSourcemap = global.isProd && config.browserify.sourcemap;
+        return stream.on('error', handleErrors)
+            .pipe(source(file))
+            .pipe(gulpif(createSourcemap, buffer()))
+            .pipe(gulpif(createSourcemap, sourcemaps.init()))
+            .pipe(gulpif(global.isProd, streamify(uglify({
+                compress: { drop_console: true }
+            }))))
+            .pipe(gulpif(createSourcemap, sourcemaps.write('./')))
+            .pipe(gulp.dest(config.scripts.dest))
+            .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true, once: true })));
+    }
 
-    gutil.log('Rebundle...');
-
-    return stream.on('error', handleErrors)
-      .pipe(source(file))
-      .pipe(gulpif(createSourcemap, buffer()))
-      .pipe(gulpif(createSourcemap, sourcemaps.init()))
-      .pipe(gulpif(global.isProd, streamify(uglify({
-        compress: { drop_console: true }
-      }))))
-      .pipe(gulpif(createSourcemap, sourcemaps.write('./')))
-      .pipe(gulp.dest(config.scripts.dest))
-      .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true, once: true })));
-  }
-
-  return rebundle();
-
+    return rebundle();
 }
 
 gulp.task('browserify', function() {
-
-  return buildScript('main.js');
-
+    return buildScript('main.js');
 });
