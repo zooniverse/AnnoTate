3c8ca7ce8c6ae44447aab6c410def9d2405f72e7
Roger Hutchings <roger@hutchin.gs>
2015-05-15 17:55:36 +0100

    Add ngLoad

--- app/modules/transcribe/transcribe.controller.js
+++ app/modules/transcribe/transcribe.controller.js
@@ -4,13 +4,21 @@ require('./transcribe.module.js')
     .controller('TranscribeCtrl', TranscribeCtrl);
 
 // @ngInject
-function TranscribeCtrl($scope, Annotations) {
+function TranscribeCtrl($rootScope, $scope, Annotations) {
 
     // ViewModel
     var vm = this;
     vm.annotations = Annotations.list();
 
     $scope.$on('next', loadNextSubject);
+
+    vm.centre = function () {
+        $rootScope.$broadcast('centre');
+    }
+
+    vm.subject = {
+        isLoaded: true
+    };
 
     function loadNextSubject() {
         Annotations.reset();
--- app/modules/transcribe/transcribe.module.js
+++ app/modules/transcribe/transcribe.module.js
@@ -1,8 +1,12 @@
 'use strict';
 
+require('ng-load');
+
 // A module for transcription interface
 module.exports = require('angular')
     .module('app.transcribe', [
+        'ngLoad',
+
         'app.annotations',
         'app.markingTools',
         'app.overlay'
