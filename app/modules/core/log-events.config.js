e4671ae7cda6b4432a69b1aaba7c7c64d28ccd8b
Roger Hutchings <roger@hutchin.gs>
2015-05-19 11:05:25 +0100

    Log all events

---
+++ app/modules/core/log-events.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('./core.module.js')
+    .config(logAllEvents);
+
+// @ngInject
+function logAllEvents($provide) {
+    $provide.decorator("$rootScope", function($delegate) {
+        var Scope = $delegate.constructor;
+        var origBroadcast = Scope.prototype.$broadcast;
+        var origEmit = Scope.prototype.$emit;
+
+        Scope.prototype.$broadcast = function() {
+            logEvent('$broadcast', arguments);
+            return origBroadcast.apply(this, arguments);
+        };
+
+        Scope.prototype.$emit = function() {
+            logEvent('$emit', arguments);
+            return origEmit.apply(this, arguments);
+        };
+        return $delegate;
+    });
+
+    function logEvent(type, args) {
+        var eventArgs = Array.prototype.slice.call(args);
+        console.log(type, eventArgs.shift(), eventArgs);
+    }
+}
+
+
