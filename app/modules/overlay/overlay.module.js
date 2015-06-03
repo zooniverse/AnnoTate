bd6bde4391e4686bb4767f0ccc62604a0fe9c84e
Roger Hutchings <roger@hutchin.gs>
2015-06-03 17:05:21 +0100

    How is angular-spinner still in here

--- app/modules/overlay/overlay.module.js
+++ app/modules/overlay/overlay.module.js
@@ -1,10 +1,7 @@
 'use strict';
 
 require('spin');
-require('angular-spinner');
 
 // A module for context menus, editing windows etc
 module.exports = require('angular')
-    .module('app.overlay', [
-        'angularSpinner'
-    ]);
+    .module('app.overlay', []);
