7bccea3d129bd2666f42ace5e9bd0cd15b194ca7
Roger Hutchings <roger@hutchin.gs>
2015-07-31 11:34:18 +0100

    Add footers

---
+++ app/modules/layout/footer/footer.constants.js
@@ -0,0 +1,110 @@
+'use strict';
+
+require('./footer.module.js')
+    .constant('FooterLinkConstants', [
+        {
+            title: 'Space',
+            projects: [
+                {
+                    name: 'Galaxy Zoo',
+                    url: 'http://www.galaxyzoo.org/'
+                },
+                {
+                    name: 'Moon Zoo',
+                    url: 'http://www.moonzoo.org/'
+                },
+                {
+                    name: 'Solar Storm Watch',
+                    url: 'http://www.solarstormwatch.com/'
+                },
+                {
+                    name: 'Planet Hunters',
+                    url: 'http://planethunters.org/'
+                },
+                {
+                    name: 'Planet Four',
+                    url: 'http://planetfour.org/'
+                },
+                {
+                    name: 'Radio Galaxy Zoo',
+                    url: 'http://radio.galaxyzoo.org/'
+                },
+                {
+                    name: 'Stardate M83',
+                    url: 'http://www.projectstardate.org/'
+                },
+                {
+                    name: 'Disk Detective',
+                    url: 'http://diskdetective.org/'
+                }
+            ]
+        },
+        {
+            title: 'Climate',
+            projects: [
+                {
+                    name: 'Old Weather',
+                    url: 'http://oldweather.org/'
+                },
+                {
+                    name: 'Cyclone Center',
+                    url: 'http://cyclonecenter.org/'
+                }
+            ]
+        },
+        {
+            title: 'Nature',
+            projects: [
+                {
+                    name: 'Whale FM',
+                    url: 'http://whale.fm/'
+                },
+                {
+                    name: 'Seafloor Explorer',
+                    url: 'http://www.seafloorexplorer.org/'
+                },
+                {
+                    name: 'Bat Detective',
+                    url: 'http://batdetective.org/'
+                },
+                {
+                    name: 'Snapshot Serengeti',
+                    url: 'http://snapshotserengeti.org/'
+                },
+                {
+                    name: 'Notes from Nature',
+                    url: 'http://notesfromnature.org/'
+                },
+                {
+                    name: 'Plankton Portal',
+                    url: 'http://planktonportal.org/'
+                }
+            ]
+        },
+        {
+            title: 'Humanities',
+            projects: [
+                {
+                    name: 'Operation War Diary',
+                    url: 'http://operationwardiary.org/'
+                },
+                {
+                    name: 'Ancient Lives',
+                    url: 'http://ancientlives.org/'
+                }
+            ]
+        },
+        {
+            title: 'Biology',
+            projects: [
+                {
+                    name: 'Cell Slider',
+                    url: 'http://cellslider.net/'
+                },
+                {
+                    name: 'Worm Watch Lab',
+                    url: 'http://wormwatchlab.org/'
+                }
+            ]
+        }
+    ]);
