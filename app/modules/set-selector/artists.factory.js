2581dea1b9d837097796d828e1fdbbaff3d1c5e8
Roger Hutchings <roger@hutchin.gs>
2015-09-02 15:41:50 +0100

    Save artist sets locally

--- app/modules/set-selector/artists.factory.js
+++ app/modules/set-selector/artists.factory.js
@@ -8,8 +8,13 @@ require('./set-selector.module.js')
 // @ngInject
 function ArtistsFactory($q, ArtistListConstants, localStorageService, zooAPIConfig, zooAPI) {
 
+    if (localStorageService.get('artistSets') === null) {
+        localStorageService.set('artistSets', {});
+    }
+
     var factory;
     var _artists = _.filter(ArtistListConstants, { active: true });
+    var _artistSets = localStorageService.get('artistSets');
 
     factory = {
         detail: detail,
@@ -18,19 +23,31 @@ function ArtistsFactory($q, ArtistListConstants, localStorageService, zooAPIConf
 
     return factory;
 
-    function detail(artistId) {
-        var artist = _.find(_artists, { artistId: artistId });
+    function _getSets(artist) {
         return zooAPI.type('subject_sets').get({
             'project_id': zooAPIConfig.project_id,
-            'metadata.artistId': parseInt(artistId, 10),
+            'metadata.artistId': parseInt(artist.artistId, 10),
             'page_size': 150
         }).then(function (sets) {
             artist.sets = sets;
+            _artistSets[artist.artistId] = sets;
+            localStorageService.set('artistSets', _artistSets);
             return artist;
         }, function (response) {
             console.error('Error getting sets', response);
             return artist;
         });
+    }
+
+    function detail(artistId) {
+        var artist = _.find(_artists, { artistId: artistId });
+        if (_artistSets[artistId]) {
+            artist.sets = _artistSets[artistId];
+            _getSets(artist);
+            return $q.when(artist);
+        } else {
+            return _getSets(artist);
+        }
     }
 
     function list(listLength) {
