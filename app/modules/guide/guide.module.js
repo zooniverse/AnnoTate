7d95bbc7e3a71bb9718f0bbde1ad290559e022cc
Roger Hutchings <roger@hutchin.gs>
2015-09-14 15:26:41 +0100

    Basic layout, content for guide

---
+++ app/modules/guide/guide.module.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = require('angular')
+    .module('app.guide', []);
---
+++ app/modules/guide/guide.routes.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var _ = require('lodash');
+
+require('./guide.module.js')
+    .config(Routes);
+
+// @ngInject
+function Routes($stateProvider, $urlRouterProvider, GuidePages) {
+
+    $stateProvider
+        .state('GuideBase', {
+            parent: 'Base',
+            url: '/guide',
+            views: {
+                'main': {
+                    templateUrl: 'guide/guide-base.html',
+                    // @ngInject
+                    controller: function ($state) {
+                        var vm = this;
+                        vm.sections = GuidePages;
+                    },
+                    controllerAs: 'vm'
+                }
+            },
+            params: {
+                hideHook: true
+            }
+        })
+        .state('GuidePage', {
+            parent: 'GuideBase',
+            url: '/:page',
+            views: {
+                'page': {
+                    templateUrl: function ($stateParams) {
+                        return 'guide/' + $stateParams.page + '.html';
+                    }
+                }
+            },
+            params: {
+                hideHook: true
+            }
+        });
+
+    $urlRouterProvider
+        .when('/guide', '/guide/overview')
+
+}
+
+
