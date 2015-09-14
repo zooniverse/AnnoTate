1d0fb2e82ae11021132d432b5d5802c6a55b099e
Roger Hutchings <roger@hutchin.gs>
2015-09-14 15:26:41 +0100

    Basic layout, content for guide

---
+++ app/modules/guide/pages.constants.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('./guide.module.js')
+    .constant('GuidePages', [
+        {
+            slug: 'overview',
+            title: 'Overview'
+        },
+        {
+            slug: 'line-by-line',
+            title: 'Line by line transcription'
+        },
+        {
+            slug: 'transcribing',
+            title: 'How to transcribe a line'
+        },
+        {
+            slug: 'images',
+            title: 'Images'
+        },
+        {
+            slug: 'blank',
+            title: 'Blank pages'
+        },
+        {
+            slug: 'keyboard',
+            title: 'Keyboard shortcuts'
+        },
+        {
+            slug: 'insertion',
+            title: 'Insertion'
+        },
+        {
+            slug: 'deletion',
+            title: 'Deletion'
+        },
+        {
+            slug: 'illegible',
+            title: 'Illegible'
+        },
+        {
+            slug: 'not-english',
+            title: 'Not English'
+        }
+    ]);
+
+
