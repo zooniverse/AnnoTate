a30ab7a2fde93c8f089eaafed5b517bb1c80baf2
Roger Hutchings <roger@hutchin.gs>
2016-04-15 15:14:39 +0100

    Move app id to app config

--- app/modules/app.config.js
+++ app/modules/app.config.js
@@ -4,7 +4,14 @@ var appInfo = require('../../package.json');
 
 var appConfig = {
     appTitle: 'AnnoTate',
-    appDescription: 'Help the Tate discover the lives of twentieth-century artists by transcribing their letters, diaries and sketchbooks.'
+    appDescription: 'Help the Tate discover the lives of twentieth-century artists by transcribing their letters, diaries and sketchbooks.',
+    app_id: (function() {
+        var APP_IDS = {
+            staging: '387d539e68b2064b254fb67f477d8933c9d859db5358661e080034511d70b98c',
+            production: '40557049c91840f89c2df75f44dbac5de2dfdfda3b44c49f2aa8f4a2381414ba',
+        };
+        return APP_IDS[process.env.NODE_ENV] || APP_IDS.staging;
+    })()
 };
 
 // @ngInject
--- app/modules/zoo-api/zooapi.config.js
+++ app/modules/zoo-api/zooapi.config.js
@@ -4,6 +4,4 @@ require('./zooapi.module.js')
     .constant('zooAPIConfig', {
         display_name: 'AnnoTate',
         project_id: '245',
-
-        app_id: '40557049c91840f89c2df75f44dbac5de2dfdfda3b44c49f2aa8f4a2381414ba'
     });
