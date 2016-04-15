0a5c18bd2267cb7d59afcdfddb6763c999e93936
Roger Hutchings <roger@hutchin.gs>
2016-04-15 15:17:25 +0100

    Rename preview task to staging, add to package.json script block

--- gulp/tasks/deploy-preview.js
+++ gulp/tasks/deploy-preview.js
@@ -4,7 +4,7 @@ var config = require('../config');
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.task('deploy-preview', function (callback) {
-    global.s3Params = config.deploy.preview;
+gulp.task('deploy-staging', function (callback) {
+    global.s3Params = config.deploy.staging;
     runSequence('production', 'deploy', callback);
 });
